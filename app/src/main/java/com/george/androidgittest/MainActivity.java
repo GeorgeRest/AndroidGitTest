fd8977b7c2368f9846682f6355bba53d387cd0bb
zxp <1727009010@qq.com>
2023-04-21 15:53:06 +0800

    sunny22

--- app/src/main/java/com/george/androidgittest/MainActivity.java
+++ app/src/main/java/com/george/androidgittest/MainActivity.java
@@ -3,6 +3,8 @@ package com.george.androidgittest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("这里是sunny第一次修改");
+        Button bt1 = (Button) findViewById(R.id.bt1);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("这里是sunny第二次修改");
+            }
+        });
+
     }
 }
