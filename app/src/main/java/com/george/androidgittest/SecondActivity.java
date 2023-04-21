2d3fd8d3469c787641ac8d22abf6d559b0adf302
Shr <shehuiren3613@gmail.com>
2023-04-21 16:20:22 +0800

    zhw3:SecondActivity 50

---
+++ app/src/main/java/com/george/androidgittest/SecondActivity.java
@@ -0,0 +1,22 @@
+package com.george.androidgittest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String second = intent.getStringExtra("second");
+        TextView textView = (TextView) findViewById(R.id.textview);
+        textView.setText(second);
+
+        //zhw
+    }
+}
